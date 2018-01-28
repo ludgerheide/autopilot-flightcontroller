bceb786197b7f94d687debc2651bc9ad3b294a08
Ludger Heide <ludger.heide@lhtechnologies.de>
2018-01-28 15:55:02 +0100

    Manual buffer sizing

--- setup/pinSetup.h
+++ setup/pinSetup.h
@@ -11,9 +11,17 @@
 
 //UART Numbers and ther functions
 #define XBEE_UART 3
+#define UART3_TX_BUFFER_SIZE (100) //Those messages should be kinda short
+#define UART3_RX_BUFFER_SIZE (100)
 #define GPS_UART 2
+#define UART2_TX_BUFFER_SIZE (82) //An NMEA msg is 82 chars long, we may have three in the buffer
+#define UART2_RX_BUFFER_SIZE (82*3)
 #define RASPI_UART 0
 #define UART0_TX_BUFFER_SIZE (512+7)
+#define UART0_RX_BUFFER_SIZE (128)
+
+#define UART1_TX_BUFFER_SIZE (1) //This uart is not in use
+#define UART1_RX_BUFFER_SIZE (1)
 
 //The Servo numbers
 #define SERVO1_PORT PORTB
